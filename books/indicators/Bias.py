f7bad2cd15e4c011603846fba71855feab40f6a6
alex <tangyongjin@gmail.com>
2023-02-03 13:34:08 +0800

    指标颜色确定

--- books/indicators/Bias.py
+++ books/indicators/Bias.py
@@ -7,10 +7,15 @@ class Bias(bt.Indicator):
     lines = ('Bias',)
     params = (('MA_period',20),)
     plotinfo = dict(subplot=True)
+    # plotlines = dict(Bias=dict(_fill_gt('Bias', ('yellow', 0.50))))
+    plotlines = dict(Bias=dict( color="yellow", alpha=0.9))
+
     
     def __init__(self):
         print("Bias __init__")
         close = self.data.close
         self.lines.Bias =(close/close)*400
+        # self.lines.Bias =1 
+        
         # super(Bias,self).__init__()
          
