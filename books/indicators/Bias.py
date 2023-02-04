995daf74e8df278ccb81fae389d69cd44cb5b3bb
alex <tangyongjin@gmail.com>
2023-02-04 18:03:28 +0800

    输出到html

--- books/indicators/Bias.py
+++ books/indicators/Bias.py
@@ -8,14 +8,18 @@ class Bias(bt.Indicator):
     params = (('MA_period',20),)
     plotinfo = dict(subplot=True)
     # plotlines = dict(Bias=dict(_fill_gt('Bias', ('yellow', 0.50))))
+    
+    plotlines = dict(
+        senkou_span_a=dict(_fill_gt=('senkou_span_b', 'g'),
+                           _fill_lt=('senkou_span_b', 'r')),
+    )
+    
     plotlines = dict(Bias=dict( color="yellow", alpha=0.9))
 
     
     def __init__(self):
         print("Bias __init__")
-        close = self.data.close
+        close = self.data.close 
         self.lines.Bias =(close/close)*400
-        # self.lines.Bias =1 
-        
         # super(Bias,self).__init__()
          
